1ceefef52db41caabfad2b1f792e818ea28a04b9
Kofikorang12 <jbaah69@gmail.com>
2025-01-11 22:01:27 +0000

    Fixing the routing issues

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  // base: "/",
   // port
   server: {
+    hmr: {
+      overlay: false,
+    },
     host: true,
     port: 80,
   },
